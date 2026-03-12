6b6813af5be49d0dbc4ad3c4791031b40c5f335f
PaulFinch <1887365+PaulFinch@users.noreply.github.com>
2026-03-12 08:40:03 +0100

    Update

--- merge.sh
+++ merge.sh
@@ -7,8 +7,8 @@ fi
 
 if [ "$#" -eq 1 ] && [ "$1" == "clean" ]; then
 	echo "Cleaning..."
-	[ -d ~/.cache/nvim ] && rm -rf ~/.cache/nvim
-	[ -d ~/.local/share/nvim ] && rm -rf ~/.local/share/nvim
+	[ -d ~/.cache/nvim ] && sudo rm -rf ~/.cache/nvim
+	[ -d ~/.local/share/nvim ] && sudo rm -rf ~/.local/share/nvim
 fi
 
 git remote -v
